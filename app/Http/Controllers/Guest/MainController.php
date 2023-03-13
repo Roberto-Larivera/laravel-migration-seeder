6c563a704ce8dd4bb6873f65319f3aab139df829
Roberto Larivera <robylariv@gmail.com>
2023-03-13 15:19:35 +0100

    Completato Milestone 2 + stampa dd

--- app/Http/Controllers/Guest/MainController.php
+++ app/Http/Controllers/Guest/MainController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Models
+use App\Models\Train;
 class MainController extends Controller
 {
     public function index()
     {
-        return view('home');
-        //dd('ok');
+        //dd(Train::all());
+        return view('home',[
+            'trains' => Train::all()
+        ]);
+        
 
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,44 @@
     <main>
        <div class="container">
         <div class="row">
+            @foreach ($trains as $item)
+            
             <div class="col text-center text-danger">
-                main
+                <ul>
+                    <li>
+                        {{ $item->id }}
+                    </li>
+                    <li>
+                        {{ $item->agency }}
+                    </li>
+                    <li>
+                        {{ $item->departure_station }}
+                    </li>
+                    <li>
+                        {{ $item->arrival_station }}
+                    </li>
+                    <li>
+                        {{ $item->departure_time }}
+                    </li>
+                    <li>
+                        {{ $item->arrival_time }}
+                    </li>
+                    <li>
+                        {{ $item->train_code }}
+                    </li>
+                    <li>
+                        {{ $item->carriages_number }}
+                    </li>
+                    <li>
+                        {{ $item->in_time }}
+                    </li>
+                    <li>
+                        {{ $item->deleted }}
+                    </li>
+                </ul>
             </div>
+            
+            @endforeach
         </div>
        </div>
     </main>
